46df23e04bf4cbd8657f4d0c6ed13dfcaf65a0a8
CodeingersCat <rocketpilot763@gmail.com>
2023-05-11 17:35:59 +0530

    Ray traced a sphere

---
+++ RayTracing/src/Renderer.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Walnut/Image.h"
+
+#include <memory>
+#include<glm/glm.hpp>
+
+class Renderer
+{
+public: 
+	Renderer() = default;
+
+	void OnResize(uint32_t Width, uint32_t Height);
+	void Render();
+
+	std::shared_ptr<Walnut::Image> GetFinalImage() const { return m_FinalImage; }
+
+private:
+	std::shared_ptr<Walnut::Image> m_FinalImage; 
+	uint32_t* m_ImageData = nullptr;
+
+	uint32_t PerPixel(glm::vec2 coord);
+};
